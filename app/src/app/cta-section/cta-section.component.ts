2dab738a78b0db4d3c8c1917b4557fadf6f70640
Rodrigo Santos <eduardo@educarmais.pt>
2023-03-17 09:56:37 +0000

    resultado da sessao 4

---
+++ app/src/app/cta-section/cta-section.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cta-section',
+  templateUrl: './cta-section.component.html',
+  styleUrls: ['./cta-section.component.css']
+})
+export class CtaSectionComponent {
+
+  constructor(private router: Router) { }
+
+  subscritor: string = 'Introduza o seu email';
+
+  agradecer():void{
+    this.subscritor = "";
+    this.router.navigate(['/sobre'],{queryParams: {param1: 'Obrigado' , param2: ' pela informação'}})
+  }
+
+
+}
